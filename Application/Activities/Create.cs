31e08b0ddf06596a9af04dc6e14334418268be23
SG <gacarlso@ucsd.edu>
2023-09-04 20:57:24 -0600

    End of section 14

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,9 @@
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -24,12 +26,26 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext __context;
-            public Handler(DataContext _context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext _context, IUserAccessor userAccessor)
             {
+            _userAccessor = userAccessor;
                 __context = _context;
             }
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await __context.Users.FirstOrDefaultAsync(x => 
+                    x.UserName == _userAccessor.GetUsername());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
+
                 __context.Activities.Add(request.Activity);
 
                 var result = await __context.SaveChangesAsync() > 0;
